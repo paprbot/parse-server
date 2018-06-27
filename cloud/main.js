7862ef6558fb2ff113841cea626caecf6930cbca
Account4All <rferzli@hotmail.com>
2018-06-27 18:56:07 +0000

    Added cloud afterSave for Posts to index AlgoliaSearch

--- cloud/main.js
+++ cloud/main.js
@@ -9,6 +9,73 @@ var indexHashtags = client.initIndex('dev_hashtags');
 var indexMeetings = client.initIndex('dev_meeetings');
 
 
+// Add and Update AlgoliaSearch post object if it's deleted from Parse
+Parse.Cloud.afterSave('Post', function(request, response) {
+ 
+  // Convert Parse.Object to JSON
+  var objectToSave = request.object.toJSON();
+  
+  //var Post = Parse.Object.extend("Post");
+  var queryPost = new Parse.Query("Post");
+  queryPost.include( ["user", "workspace", "project"] );
+  queryPost.select(["user", "post_type", "privacy","text", "likesCount", "CommentCount", "updatedAt", "objectId", "topIntent", "hasURL","hashtags", "mentions",  "workspace.workspace_name", "workspace.workspace_url", "project.name", "project.type", "project.archive"]);
+  queryPost.equalTo("objectId", objectToSave.objectId);
+  
+  console.log("Request: " + JSON.stringify(request));
+  console.log("objectID: " + objectToSave.objectId);
+  console.log("objectID: " + objectToSave.user.objectId);
+  
+  queryPost.first({
+    success: function(post) {
+      // Successfully retrieved the object.
+      console.log("ObjectToSave: " + JSON.stringify(post));
+      
+      // Convert Parse.Object to JSON
+      post = post.toJSON();
+      
+      // Specify Algolia's objectID with the Parse.Object unique ID
+      post.objectID = post.objectId;
+      
+      // Add or update object
+      indexPosts.saveObject(post, function(err, content) {
+        if (err) {
+          throw err;
+        }
+        console.log('Parse<>Algolia object saved');
+        response.success(); 
+        
+      });
+      
+    },
+    error: function(error) {
+      alert("Error: " + error.code + " " + error.message);
+    }
+  });
+  
+  
+});
+
+
+// Add and Update AlgoliaSearch user object if it's deleted from Parse
+Parse.Cloud.afterSave('_User', function(request) {
+  
+  // Convert Parse.Object to JSON
+  var objectToSave = request.object.toJSON();
+  
+  // Specify Algolia's objectID with the Parse.Object unique ID
+  objectToSave.objectID = objectToSave.objectId;
+  
+  // Add or update object
+  indexUsers.saveObject(objectToSave, function(err, content) {
+    if (err) {
+      throw err;
+    }
+    console.log('Parse<>Algolia object saved');
+  });
+});
+
+
+// Delete AlgoliaSearch post object if it's deleted from Parse
 Parse.Cloud.afterDelete('Post', function(request) {
   
   // Get Algolia objectID
@@ -23,6 +90,7 @@ Parse.Cloud.afterDelete('Post', function(request) {
   });
 });
 
+// Delete AlgoliaSearch user object if it's deleted from Parse
 Parse.Cloud.afterDelete('_User', function(request) {
   
   // Get Algolia objectID
