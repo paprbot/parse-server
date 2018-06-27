0e57b73db161fd50e6b9280d81f9dfe935593421
Account4All <rferzli@hotmail.com>
2018-06-27 01:43:38 +0000

    Added AfterDelete for Posts and Users Indexs in AlgoliaSearch

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,40 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+var algoliasearch = require('algoliasearch');
+var client = algoliasearch('K3ET7YKLTI', '67085f00b6dbdd989eddc47fd1975c9c');
+
+// Initialize the Algolia Search Indexes for posts, users, hashtags and meetings
+var indexPosts = client.initIndex('dev_posts');
+var indexUsers = client.initIndex('dev_users');
+var indexHashtags = client.initIndex('dev_hashtags');
+var indexMeetings = client.initIndex('dev_meeetings');
+
+
+Parse.Cloud.afterDelete('Post', function(request) {
+  
+  // Get Algolia objectID
+  var objectID = request.object.id;
+  
+  // Remove the object from Algolia
+  indexPosts.deleteObject(objectID, function(err, content) {
+    if (err) {
+      throw err;
+    }
+    console.log('Parse<>Algolia object deleted');
+  });
 });
+
+Parse.Cloud.afterDelete('_User', function(request) {
+  
+  // Get Algolia objectID
+  var objectID = request.object.id;
+  
+  // Remove the object from Algolia
+  indexUsers.deleteObject(objectID, function(err, content) {
+    if (err) {
+      throw err;
+    }
+    console.log('Parse<>Algolia object deleted');
+  });
+});
+
+
