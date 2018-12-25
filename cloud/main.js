5afaae11ff8ef96a050ef02a576f069e7ac678a0
Rajendra <rajendra@bluelabellabs.com>
2018-12-25 19:13:09 +0530

    changed mail credentials

--- cloud/main.js
+++ cloud/main.js
@@ -2406,17 +2406,11 @@ Parse.Cloud.define("sendEmail", function(request, response) {
   var transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com', 
     port: 587,
+    secure: false,
     auth: {
       user: 'testmail.team5@gmail.com',
       pass: '123team5'
     }
-    // host: 'smtp.mandrillapp.com', 
-    // port: 587,
-    // secure: false,
-    // auth: {
-    //   user: 'Papr, Inc.',
-    //   pass: 'fCWj2D9rBGfJsaU1RSFU5w'
-    // }
   });
   var readHTMLFile = function(path, callback) {
       fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
