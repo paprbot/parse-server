a27f37e178fd5b1da9c1d0d9d9eca35d1b9c7b0c
Nishith Sheth <nishith.sheth@tatvasoft.com>
2018-12-31 17:07:19 +0530

    Port changes.

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.get('/deeplink', deeplink({
 );
 
 var httpServer = require('http').createServer(app);
-var port = process.env.PORT || 1337;
-//var port = 1337;
+//var port = process.env.PORT || 1337;
+var port = 1337;
 
 
 httpServer.listen(port, function() {
