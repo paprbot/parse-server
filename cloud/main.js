5cdc2181f72364c5607d07c3778f53e87985be67
Rajendra <rajendra@bluelabellabs.com>
2019-01-01 10:41:57 +0530

    testing push notification data

--- cloud/main.js
+++ cloud/main.js
@@ -2487,7 +2487,7 @@ Parse.Cloud.define("sendNotification", function(request, response) {
   // var tokenArray = new Array();
   query.find({
     success: function(results) {
-      response.success(results[0]);
+      response.success(results[0].result.userTo);
       /*var counter = require('counter'),
       count = counter(0, { target: Object.keys(results).length - 1, once: true }),
       i, l = Object.keys(results).length - 1;
