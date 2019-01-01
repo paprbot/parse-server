d082f962d9cf8335a7d48a9b3e43a79716d5052a
Rajendra <rajendra@bluelabellabs.com>
2019-01-01 13:47:10 +0530

    testing : fetching data using async

--- cloud/main.js
+++ cloud/main.js
@@ -2471,7 +2471,7 @@ Parse.Cloud.define("sendEmail", function(request, response) {
   }
 });
 
-Parse.Cloud.define("sendNotification", function(request, response) {
+Parse.Cloud.define("sendNotification", async (request, response) => {
   const pn = PushNotification({
     apn: {
       cert: path.resolve('Papr-Development-APNS.pem'),
@@ -2484,22 +2484,20 @@ Parse.Cloud.define("sendNotification", function(request, response) {
   var Notification = Parse.Object.extend('Notification');
   var query = new Parse.Query(Notification);
   query.include('userTo.deviceToken');
-  query.find({
-    success: function(results) {
-      var tokenArray = new Array();
-      for (let i = 0; i < results.length; i++) {
-        tokenArray.push({
-          message: results[i].get("message"),
-          deviceToken: results[i].get("userTo").get("deviceToken"),
-        });
-      }
-      response.success(tokenArray);
-    },
-    error: function(e) {
-      console.error(e);
-      response.error(e);
+  try {
+    const results = await query.find();
+    var tokenArray = new Array();
+    for (let i = 0; i < results.length; i++) {
+      tokenArray.push({
+        message: results[i].get("message"),
+        deviceToken: results[i].get("userTo").get("deviceToken"),
+      });
     }
-  });
+    response.success(tokenArray);
+  } catch (error) {
+    console.log(error);
+    response.error(e);
+  }
 });
 
 Parse.Cloud.define('sendStaticPushNotification', (request, response) => {
