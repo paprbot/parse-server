38f1b312765bb54b94ec4ec37db47e18bfcf56c5
Rajendra Padhiyar <rajendra.padhiyar@tatvasoft.com>
2019-01-03 15:53:50 +0530

    remove live query from main

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 module.exports = {
   server: {},
-  liveQuery: {
-        classNames: ['PostQuestionMessage'],
-        redisURL: 'redis://40.122.150.203:1337'
-    },
   dashboard: {},
   storage: {},
   push: {}
