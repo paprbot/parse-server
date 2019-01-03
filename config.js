a91bb7d2ed363c75769f1b15ad9850dc33d1b953
Rajendra Padhiyar <rajendra.padhiyar@tatvasoft.com>
2019-01-03 16:07:24 +0530

    Add Redis URL

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
   server: {},
+  liveQuery: {
+        classNames: ['PostQuestionMessage'],
+        redisURL: 'redis://40.122.150.203:1337'
+    },
   dashboard: {},
   storage: {},
   push: {}
