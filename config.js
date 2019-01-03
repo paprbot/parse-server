43f06ecd590b4cf0401b945c86bafcc293f0de5c
Rajendra Padhiyar <rajendra.padhiyar@tatvasoft.com>
2019-01-03 20:52:46 +0530

    Add port for redisURL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   server: {},
   liveQuery: {
         classNames: ['PostQuestionMessage'],
-        redisURL: 'paprtest.redis.cache.windows.net'
+        redisURL: 'paprtest.redis.cache.windows.net:6379'
     },
   dashboard: {},
   storage: {},
