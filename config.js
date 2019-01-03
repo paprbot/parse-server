b246ed7cc853a732b0f4c07d3b62ebffe212ec6d
Rajendra Padhiyar <rajendra.padhiyar@tatvasoft.com>
2019-01-03 21:18:38 +0530

    Add Redis URL

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ module.exports = {
   server: {},
   liveQuery: {
         classNames: ['PostQuestionMessage'],
-        redisURL: 'redis://paprtest.redis.cache.windows.net:6380,password=LGSn+cOIPeASKKw3QGiOCY5hhH63FckdqtjuuerF6P0=,ssl=True,abortConnect=False'
+        //redisURL: 'redis://paprtest.redis.cache.windows.net:6380,password=LGSn+cOIPeASKKw3QGiOCY5hhH63FckdqtjuuerF6P0=,ssl=True,abortConnect=False'
+        redisURL: 'redis://paprtest.redis.cache.windows.net:6379'
     },
   dashboard: {},
   storage: {},
