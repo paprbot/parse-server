78857085e66dc82f4e56fb57415b198e60b508d1
Rajendra Padhiyar <rajendra.padhiyar@tatvasoft.com>
2019-01-03 16:19:44 +0530

    reverted redis URL

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
   server: {},
   liveQuery: {
-        classNames: ['PostQuestionMessage'],
-        redisURL: 'redis://40.122.150.203:6379'
+        classNames: ['PostQuestionMessage']
     },
   dashboard: {},
   storage: {},
