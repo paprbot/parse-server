44f7e9d6af846fb8c0a3283f1b33d030357b90d8
Rajendra Padhiyar <rajendra.padhiyar@tatvasoft.com>
2019-01-04 11:52:28 +0530

    updated config.js

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ module.exports = {
   server: {  
   liveQuery: {
         classNames: ['PostQuestionMessage'],
+        redisURL: 'redis://user:LGSn+cOIPeASKKw3QGiOCY5hhH63FckdqtjuuerF6P0=@paprtest.redis.cache.windows.net:6379',
+        logLevel:'VERBOSE'
     }
 },
   dashboard: {},
