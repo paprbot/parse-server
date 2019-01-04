ddbcf0a8c55404966b9afe1ccc26aad6020dd7a1
Rajendra Padhiyar <rajendra.padhiyar@tatvasoft.com>
2019-01-04 11:20:41 +0530

    updated config.js

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  server: {},
+  server: {  
   liveQuery: {
         classNames: ['PostQuestionMessage'],
         //redisURL: 'redis://paprtest.redis.cache.windows.net:6380,password=LGSn+cOIPeASKKw3QGiOCY5hhH63FckdqtjuuerF6P0=,ssl=True,abortConnect=False'
         redisURL: 'redis://user:LGSn+cOIPeASKKw3QGiOCY5hhH63FckdqtjuuerF6P0=@paprtest.redis.cache.windows.net:6379',
         logLevel:'VERBOSE'
-    },
+    }
+},
   dashboard: {},
   storage: {},
   push: {}
