5f9c4ac389ba2197429e0277b62205a7367728be
Rajendra <bluelabeldeveloper1>
2019-01-04 16:30:14 +0530

    change in the index

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var config = parseServerConfig(__dirname);
 // Modify config as necessary before initializing parse server & dashboard
 var app = express();
 app.use('/public', express.static(__dirname + '/public'));
-app.use('/parse', new ParseServer(config.server));
+//app.use('/parse', new ParseServer(config.server));
 //app.use('/parse-dashboard', ParseDashboard(config.dashboard, true));
 
 // need to switch insecure http off once we enable ssl
@@ -30,6 +30,22 @@ liveQuery: {
 var mountPath = '/parse';
 app.use(mountPath, api);
 */
+
+var api = new ParseServer(
+{
+    databaseURI: 'mongodb://paprvmdatabase.westus2.cloudapp.azure.com:27017/parse',
+    appId: "671e705a-f735-4ec0-8474-15899a475440",
+    masterKey: "f24d6630-a35a-4db8-9fc7-6a851042bfd6",
+    //fileKey: "",
+    serverURL: "https://parseserverwest.azurewebsites.net/parse",
+    liveQuery: {
+        classNames: ['PostQuestionMessage'],
+        redisURL: 'redis://user:LGSn+cOIPeASKKw3QGiOCY5hhH63FckdqtjuuerF6P0=@paprtest.redis.cache.windows.net:6379'
+       // logLevel:'VERBOSE'
+    }
+});
+app.use('/parse', api);
+
 app.use('/parse-dashboard', ParseDashboard(config.dashboard, {allowInsecureHTTP: true}));
 app.get('/deeplink', deeplink({
         fallback: 'https://www.facebook.com/',
@@ -38,6 +54,7 @@ app.get('/deeplink', deeplink({
     })
 );
 
+/*
 var httpServer = require('http').createServer(app);
 var port = process.env.PORT || 1337;
 //var port = 1337;
@@ -50,8 +67,8 @@ console.log('Parse Server running at ${port}');
 // console.log('Parse Server running at ${config.server.serverURL}');
 // });
 ParseServer.createLiveQueryServer(httpServer);
+*/
 
-
-// app.listen(process.env.PORT || url.parse(config.server.serverURL).port, function () {
-//   console.log(`Parse Server running at ${config.server.serverURL}`);
-// });
+ app.listen(process.env.PORT || url.parse(config.server.serverURL).port, function () {
+   console.log(`Parse Server running at ${config.server.serverURL}`);
+ });
