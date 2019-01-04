9a2ae9157be07d56214b3731875fda7e61735bc3
Rajendra <bluelabeldeveloper1>
2019-01-04 17:33:29 +0530

    change in the config

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  server: {},
-  dashboard: {},
-  storage: {},
-  push: {}
+    server: {
+        liveQuery: {
+            classNames: ['PostQuestionMessage'],
+            redisURL: 'redis://user:LGSn+cOIPeASKKw3QGiOCY5hhH63FckdqtjuuerF6P0=@paprtest.redis.cache.windows.net:6379',
+            logLevel: 'VERBOSE'
+        }
+    },
+    dashboard: {},
+    storage: {},
+    push: {}
 }
