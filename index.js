c579488f5878823afd9e9d11eca0c7f6d374f7a2
Rajendra <bluelabeldeveloper1>
2019-01-04 16:13:31 +0530

    remove redisurl

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ var api = new ParseServer(
     serverURL: "https://parseserverwest.azurewebsites.net/parse",
     liveQuery: {
         classNames: ['PostQuestionMessage'],
-        redisURL: 'redis://user:LGSn+cOIPeASKKw3QGiOCY5hhH63FckdqtjuuerF6P0=@paprtest.redis.cache.windows.net:6379',
-        logLevel:'VERBOSE'
+     //   redisURL: 'redis://user:LGSn+cOIPeASKKw3QGiOCY5hhH63FckdqtjuuerF6P0=@paprtest.redis.cache.windows.net:6379',
+     //   logLevel:'VERBOSE'
     }
 });
 app.use('/parse', api);
